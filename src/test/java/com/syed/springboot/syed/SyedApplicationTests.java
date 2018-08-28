eeb1608629a11669f5bd5fc2b9933f116572ecbe
SYED JALEELUDDIN <jaleel135282@gmail.com>
2018-08-28 10:30:20 +0530

    Add files via upload

---
+++ src/test/java/com/syed/springboot/syed/SyedApplicationTests.java
@@ -0,0 +1,16 @@
+package com.syed.springboot.syed;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SyedApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
